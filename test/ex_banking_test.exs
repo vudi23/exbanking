8c603b259123b178026d8a534adb9ce4b0d7511d
vudic <56545508+vudi23@users.noreply.github.com>
2024-09-18 11:51:30 +0200

    Test create user

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -1,3 +1,25 @@
 defmodule ExBankingTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  import ExBanking
+
+  setup do
+    Application.stop(:ex_banking)
+    Application.start(:ex_banking)
+  end
+
+  describe "create_user" do
+    test "can create user" do
+      assert create_user("valid_user_name") == :ok
+    end
+
+    test "returns error with invalid user name format" do
+      assert create_user(123) == {:error, :wrong_arguments}
+    end
+
+    test "returns error when already registered" do
+      create_user("same_user_name")
+      assert create_user("same_user_name") == {:error, :user_already_exists}
+    end
+  end
 end
