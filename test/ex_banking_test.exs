9b5de64f9d5eea4965237ae9ac06a78dfb3975d2
vudic <56545508+vudi23@users.noreply.github.com>
2024-09-18 12:31:29 +0200

    Test deposit

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -22,4 +22,44 @@ defmodule ExBankingTest do
       assert create_user("same_user_name") == {:error, :user_already_exists}
     end
   end
+
+  describe "deposit" do
+    test "created user can deposit currency" do
+      create_user("user")
+      assert deposit("user", 100, "usd") == {:ok, 100}
+    end
+
+    test "nonexisting user can't deposit curency" do
+      assert deposit("nonexisting_user", 100, "usd") == {:error, :user_does_not_exist}
+    end
+
+    test "can't deposit with invalid currency input" do
+      create_user("user")
+      assert deposit("user", 100, 100) == {:error, :wrong_arguments}
+      assert deposit("user", "usd", "usd") == {:error, :wrong_arguments}
+    end
+
+    test "depositing adds to existing currency amount" do
+      create_user("user")
+      assert deposit("user", 100, "usd") == {:ok, 100}
+      assert deposit("user", 100.50, "usd") == {:ok, 200.5}
+    end
+
+    test "depositing is case sensitive to currency" do
+      create_user("user")
+      assert deposit("user", 100, "usd") == {:ok, 100}
+      assert deposit("user", 100, "USD") == {:ok, 100}
+    end
+
+    test "truncates amount input after two digits" do
+      create_user("user")
+      assert deposit("user", 100.1111, "usd") == {:ok, 100.11}
+      assert deposit("user", 100.9999, "eur") == {:ok, 100.99}
+    end
+
+    test "can't deposit nothing" do
+      create_user("user")
+      assert deposit("user", 0, "usd") == {:error, :wrong_arguments}
+    end
+  end
 end
