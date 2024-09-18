0054d6f8abdbdb64a982f30774d00b85e5af75ba
vudic <56545508+vudi23@users.noreply.github.com>
2024-09-18 11:14:44 +0200

    Cleanup autogenerated comments

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,21 +1,4 @@
 defmodule ExBanking do
-  @moduledoc """
-  Documentation for `ExBanking`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def start_link do
     Supervisor.start_link(
       [
