ed77ed24dfceacbbf956f520b4051c79bc5099b7
vudic <56545508+vudi23@users.noreply.github.com>
2024-09-18 11:45:43 +0200

    Implement create user

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,4 +1,20 @@
 defmodule ExBanking do
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) do
+    with :ok <- valid_string_input(user),
+         :ok <- insert_new_user(user),
+         do: :ok
+  end
+
+  defp valid_string_input(param) do
+    if is_binary(param), do: :ok, else: {:error, :wrong_arguments}
+  end
+
+  defp insert_new_user(user) do
+    with {:error, :already_exists} <- ConCache.insert_new(:bank, user, %{}),
+         do: {:error, :user_already_exists}
+  end
+
   def start_link do
     Supervisor.start_link(
       [
